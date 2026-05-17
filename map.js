bf162fd3e64ca81b3e732a1da5232dbd6e47d865
sunBrian-UCSD <brsun@ucsd.edu>
2026-05-16 20:59:34 -0700

    Update map.js

--- map.js
+++ map.js
@@ -50,7 +50,7 @@ map.on('load', async () => {
   // Fetch station data
   let jsonData;
   try {
-    const jsonurl = INPUT_BLUEBIKES_CSV_URL;
+    const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
     const jsonData = await d3.json(jsonurl);
     console.log('Loaded JSON Data:', jsonData);
   } catch (error) {
