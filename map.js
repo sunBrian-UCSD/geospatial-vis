959e73ab86ac07bb39b194c7fbfb5e72bcb5e07e
sunBrian-UCSD <brsun@ucsd.edu>
2026-05-16 19:57:40 -0700

    Update map.js

--- map.js
+++ map.js
@@ -3,3 +3,23 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+map.on('load', async () => {
+  
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  map.addLayer({
+    id: 'bike-lanes',
+    type: 'line',
+    source: 'boston_route',  // 👈 matches the addSource ID
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+
+});
