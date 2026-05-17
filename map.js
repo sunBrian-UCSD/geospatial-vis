6ea07ff208fb4feb005d67f3d458557f0e80ef67
sunBrian-UCSD <brsun@ucsd.edu>
2026-05-16 20:07:54 -0700

    Fixed mapbox display in webpage

--- map.js
+++ map.js
@@ -1,3 +1,15 @@
+import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYnJzdW4iLCJhIjoiY21wOTV5aXU2MGVsNzJycHozc3lmMTY5eCJ9.dfPg3KyLFKfmjd0stuYBnA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
 map.on('load', async () => {
   
   // Boston bike lanes
