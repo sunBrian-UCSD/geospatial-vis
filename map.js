879b382500d36a69ad35d6aad5f9ec4cd71f51f9
sunBrian-UCSD <brsun@ucsd.edu>
2026-05-16 20:00:47 -0700

    Update map.js

--- map.js
+++ map.js
@@ -1,11 +1,6 @@
-// Import Mapbox as an ESM module
-import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
-
-// Check that Mapbox GL JS is loaded
-console.log('Mapbox GL JS Loaded:', mapboxgl);
-
 map.on('load', async () => {
   
+  // Boston bike lanes
   map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
@@ -14,7 +9,24 @@ map.on('load', async () => {
   map.addLayer({
     id: 'bike-lanes',
     type: 'line',
-    source: 'boston_route',  // 👈 matches the addSource ID
+    source: 'boston_route',
+    paint: {
+      'line-color': 'green',
+      'line-width': 3,
+      'line-opacity': 0.4,
+    },
+  });
+
+  // Cambridge bike lanes
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
     paint: {
       'line-color': 'green',
       'line-width': 3,
